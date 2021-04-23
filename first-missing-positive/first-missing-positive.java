2c0cf0bbed4d8beb1b40f1389c4eba5bdcc60c9e
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2021-04-22 22:22:04 -0400

    Time: 0 ms (100.00%), Space: 36.5 MB (75.70%) - LeetHub

--- first-missing-positive/first-missing-positive.java
+++ first-missing-positive/first-missing-positive.java
@@ -1,23 +1,38 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        for (int i=0; i<nums.length; i++) {
-            if (nums[i] <= 0) {
-                nums[i] = 301;
+    public int firstMissingPositive(int[] A) {
+        boolean onePresent = false;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 1) {
+                onePresent = true;
+            }
+
+            if (A[i] <= 0) {
+                A[i] = 1;
             }
         }
-        
-        for (int i=0; i<nums.length; i++) {
-            int index = Math.abs(nums[i]) - 1;
-            if (index < nums.length && nums[index] > 0) {
-                nums[index] = -1 * nums[index];
+
+       if (!onePresent) {
+           return 1;
+       }
+
+
+
+        for (int i = 0; i < A.length; i++) {
+            
+            int num = Math.abs(A[i]);
+
+            if (num <= A.length && A[num - 1] > 0) {
+                A[num - 1] = -A[num - 1];
             }
         }
-        // System.out.println(Arrays.toString(nums));
-        for (int i=0; i<nums.length; i++) {
-            if (nums[i] > 0) {
-                return i+1;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] >= 0) {
+                return i + 1;
             }
-        } 
-        return nums.length + 1;
+        }
+
+        return A.length + 1;
     }
 }
