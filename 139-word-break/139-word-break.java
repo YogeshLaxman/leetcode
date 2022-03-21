8066972f3d8c5a0ea2a0cae650b6cdac2e43d166
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-03-20 18:19:33 -0700

    Time: 29 ms (5.25%), Space: 47.6 MB (19.45%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,27 +1,29 @@
 class Solution {
-    int[] memo;
     public boolean wordBreak(String s, List<String> wordDict) {
-        memo = new int[s.length()];
-        Arrays.fill(memo, -1);
-        return wordBreak(s, new HashSet<>(wordDict), 0);
+        Map<String, Boolean> cache = new HashMap<>();
+        return wordBreakDP(s, wordDict, cache);
     }
-    
-    public boolean wordBreak(String s, Set<String> wordDict, int index) {
-        if (index == s.length()) return true;
-        if (memo[index] != -1) return memo[index] == 1;
-        StringBuilder sb = new StringBuilder();
-        boolean ans = false;
-        for (int i=index; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-            if (wordDict.contains(sb.toString())) {
-                ans = ans || wordBreak(s, wordDict, i+1);
-            }
-            if (ans) {
-                break;
+    private boolean wordBreakDP(String s, List<String> wordDict, Map<String, Boolean> cache) {
+        if(cache.containsKey(s)) {
+            return cache.get(s);
+        }
+        String currentSubstring = "";
+        for(int i=0; i < s.length(); i++) {
+            currentSubstring = currentSubstring + s.charAt(i);
+            if(wordDict.contains(currentSubstring)) {
+                if(i+1 == s.length()) {
+                    cache.put(s, true);
+                    // System.out.println("here");
+                    return true;
+                } else {
+                   if(wordBreakDP(s.substring(i+1), wordDict, cache)) {
+                        cache.put(s.substring(i+1), true);
+                        return true;   
+                   }
+                }
             }
         }
-        memo[index] = ans ? 1 : 0;
-        
-        return ans;
+        cache.put(s, false);
+        return false;
     }
 }
