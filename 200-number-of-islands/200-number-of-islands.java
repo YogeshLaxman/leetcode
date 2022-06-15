8200bb7b27ef755a5864c55c5b47fb7ee2164fa0
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-14 19:17:55 -0700

    Time: 9 ms (23.89%), Space: 56.3 MB (67.56%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,3 +1,13 @@
+class Coordinate {
+    int r;
+    int c;
+    
+    Coordinate(int r, int c) {
+        this.r = r;
+        this.c = c;
+    } 
+}
+
 class Solution {
     int[][] directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
     
@@ -9,7 +19,7 @@ class Solution {
             for (int j=0; j<n; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
                     numberOfIslands++;
-                    dfs(grid, i, j, visited);
+                    bfs(grid, i, j, visited);
                 }
             }
         }
@@ -17,17 +27,26 @@ class Solution {
         return numberOfIslands;
     }
     
-    private void dfs(char[][] grid, int i, int j, boolean[][] visited) {
+    private void bfs(char[][] grid, int i, int j, boolean[][] visited) {
         visited[i][j] = true;
         int m = grid.length, n = grid[0].length;
         
-        for (int dir[]: directions) {
-            int x = i + dir[0];
-            int y = j + dir[1];
+        Queue<Coordinate> queue = new LinkedList<Coordinate>();
+        queue.add(new Coordinate(i, j));
+        
+        while (!queue.isEmpty()) {
+            Coordinate coordinate = queue.poll();
             
-            if (x >= 0 && x < m && y >=0 && y < n && grid[x][y] == '1' && !visited[x][y]) {
-                dfs(grid, x, y, visited);
-            }
+            for (int dir[]: directions) {
+                int x = coordinate.r + dir[0];
+                int y = coordinate.c + dir[1];
+
+                if (x >= 0 && x < m && y >=0 && y < n && grid[x][y] == '1' && !visited[x][y]) {
+                    visited[x][y] = true;
+                    queue.add(new Coordinate(x, y));
+                }
         }
+        }
+        
     } 
 }
