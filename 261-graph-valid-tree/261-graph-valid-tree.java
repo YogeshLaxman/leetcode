e93bfddfab8812a7b4385cf39777969114de8218
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-15 20:23:03 -0700

    Time: 4 ms (50.46%), Space: 47.4 MB (30.15%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,49 +1,34 @@
-class UnionFind {
-    int[] parent;
-    
-    UnionFind(int n) {
-        parent = new int[n];
-        Arrays.fill(parent, -1);
-    }
-    
-    int find(int x) {
-        if (parent[x] < 0) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    void union(int x, int y) {
-        if (Math.abs(parent[x]) > Math.abs(parent[y])) {
-            parent[x] += parent[y];
-            parent[y] = x;
-        } else {
-            parent[y] += parent[x];
-            parent[x] = y;
-        }
-    }
-    
-    int getNumberOfDistincSets() {
-        int count = 0;
-        for (int p: parent) {
-            if (p < 0) count++;
-        }
-        
-        return count;
-    }
-}
-
 class Solution {
     public boolean validTree(int n, int[][] edges) {
         if (edges.length != n-1) return false;
         
-        UnionFind uf = new UnionFind(n);
-        for (int[] edge: edges) {
-            int u = uf.find(edge[0]);
-            int v = uf.find(edge[1]);
-            
-            if (u == v) return false;
-            uf.union(u, v);
+        Map<Integer, Set<Integer>> adjList = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+        
+        for (int i=0; i<n; i++) {
+            adjList.put(i, new HashSet<>());
         }
         
-        return uf.getNumberOfDistincSets() == 1;
+        for (int[] edge: edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        visited.add(0);
+        
+        while (!queue.isEmpty()) {
+            int nextNode = queue.poll();
+            
+            for (int neighbor: adjList.get(nextNode)) {
+                if (!visited.contains(neighbor)) {
+                    queue.add(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+        
+        return visited.size() == n;
     }
 }
