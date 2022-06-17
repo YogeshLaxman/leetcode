f2bad047581da7bb6c0a35e4c6158872406c2884
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-16 23:09:19 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (32.74%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,13 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n+1];
-        Arrays.fill(memo, -1);
-        return climbStairs(n, memo);
-    }
-    
-    private int climbStairs(int n, int[] memo) {
-        if (n <= 1) return 1;
-        if (memo[n] != -1) return memo[n];
-        return memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+        if (n <= 2) return n;
+        int firstTerm = 1, secondTerm = 2;
+        
+        for (int i=3; i<=n; i++) {
+            int current = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = current;
+        }
+        
+        return secondTerm;
     }
 }
