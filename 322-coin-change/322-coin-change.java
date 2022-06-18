fe5ccc02060eb6669713d2972b3cd3255be1df36
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-18 15:52:54 -0700

    Time: 37 ms (35.95%), Space: 45.4 MB (41.84%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,32 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] memo = new int[coins.length+1][amount+1];
-        for (int[] item: memo) {
-            Arrays.fill(item, -1);
-        }
-        int min = Integer.MAX_VALUE;
+        int[] memo = new int[amount+1];
+        int min = coinChange(coins, amount, memo);
         
-        for (int i=0; i<coins.length; i++) {
-            min = Math.min(coinChange(coins, amount, i, memo), min);
-        }
         return min == Integer.MAX_VALUE ? -1 : min;
     }
     
-    private int coinChange(int[] coins, int amount, int index, int[][] memo) {
+    private int coinChange(int[] coins, int amount, int[] memo) {
         if (amount == 0) return 0;
         if (amount < 0) return Integer.MAX_VALUE;
-        if (index > coins.length) return Integer.MAX_VALUE;
         
-        if (memo[index][amount] != -1) {
-            return memo[index][amount];
+        if (memo[amount] != 0) {
+            return memo[amount];
         }
         
         int min = Integer.MAX_VALUE;
-        
-        for (int i=index; i<coins.length; i++) {
-            min = Math.min(coinChange(coins, amount - coins[index], i, memo), min);
+        for (int i=0; i<coins.length; i++) {
+            min = Math.min(coinChange(coins, amount - coins[i], memo), min);
         }
         
-        return memo[index][amount] = min == Integer.MAX_VALUE ? Integer.MAX_VALUE: min + 1;
+        return memo[amount] = min == Integer.MAX_VALUE ? Integer.MAX_VALUE: min + 1;
     }
 }
