e4b2cb13f59ce04740ab8a7311356f6ca12c22d0
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-19 13:55:36 -0700

    Time: 3 ms (31.58%), Space: 41.8 MB (26.80%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,5 +1,6 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
         int[] dp = new int[target+1];
         dp[0] = 1;
         
@@ -7,6 +8,8 @@ class Solution {
             for (int num: nums) {
                 if (i - num >= 0) {
                     dp[i] += dp[i-num];
+                } else {
+                    break;
                 }
             }
         }
