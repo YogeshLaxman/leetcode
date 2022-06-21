aa451a7dcd6f55d7d6621f79f9fc3441590b30b4
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-20 21:15:42 -0700

    Time: 0 ms (100.00%), Space: 41.3 MB (22.37%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        
+        for (int i=0; i<m-1; i++) {
+            for (int j=n-2; j>=0; j--) {
+                dp[j] = dp[j] + dp[j+1];
+            }
+            // System.out.println(Arrays.toString(dp));
+        }
+        
+        return dp[0];
+    }
+}
