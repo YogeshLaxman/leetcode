028ae4c1fdcfabad13b3f7bb0f49a36f0e55978d
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-20 20:50:00 -0700

    Time: 2 ms (66.37%), Space: 42.6 MB (30.15%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,34 +1,25 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s.charAt(0) == '0') return 0;
         int n = s.length();
-        int start = 0;
+        if (s.charAt(0) == '0') return 0;
+        if (n == 1) return 1;
+
+        int last = s.charAt(n-1) == '0' ? 0 : 1;
+        int lastPlusOne = 1;
         
-        return numDecodingsDP(s);
-    }
-    
-    private int numDecodingsDP(String s) {
-        // System.out.println("Called");
-        if (s.length() == 1) return 1;
-        int[] memo = new int[s.length()];
-        Arrays.fill(memo, -1);
-        return numDecodingsDP(s, 0, memo);
-    }
-    
-    private int numDecodingsDP(String s, int index, int[] memo) {
-        if (index >= s.length()) return 1;
-        if (s.charAt(index) == '0') return 0;
-        if (memo[index] != -1) return memo[index];
-        
-        int ans = numDecodingsDP(s, index+1, memo);
-        if (index+1 < s.length()) {
-            int num = (s.charAt(index) - '0') * 10 + (s.charAt(index+1) - '0');
-            if (num >= 1 && num <= 26) {
-                ans += numDecodingsDP(s, index+2, memo);
-            }    
+        for (int i=n-2; i>=0; i--) {
+            int current = 0;
+            if (s.charAt(i) != '0') {
+                current = last;
+                if (Integer.parseInt(s.substring(i, i+2)) <= 26) {
+                    current += lastPlusOne;
+                } 
+            }
+            
+            lastPlusOne = last;
+            last = current;
         }
         
-        
-        return memo[index] = ans;
+        return last;
     }
 }
