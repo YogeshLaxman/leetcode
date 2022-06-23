75e006f48d478109d5f1230a7416cc7101404520
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-06-23 00:02:16 -0700

    Time: 13 ms (82.49%), Space: 50.2 MB (67.53%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,3 +1,13 @@
+class Element {
+    int number;
+    int frequency;
+    
+    Element(int n, int f) {
+        this.number = n;
+        this.frequency = f;
+    }
+}
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> freqMap = new HashMap<>();
@@ -6,26 +16,57 @@ class Solution {
             freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
         }
         
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b) -> a.getValue() - b.getValue());
-        
+        // 1, 1, 2, 3
+        Element[] elements = new Element[freqMap.size()];
+        int index=0;
         for (Map.Entry<Integer, Integer> entry: freqMap.entrySet()) {
-            if (pq.size() < k) {
-                pq.add(entry);
-            } else {
-                Map.Entry<Integer, Integer> min = pq.peek();
-                if (min.getValue() < entry.getValue()) {
-                    pq.poll();
-                    pq.add(entry);
-                }
-            }
+            elements[index++] = new Element(entry.getKey(), entry.getValue());
+            // System.out.println(elements[index-1].number + ":" + elements[index-1].frequency);
         }
         
-        int[] ans = new int[k];
+        quickSelect(elements, 0, elements.length-1, k-1);
         
-        for (int i=k-1; i>=0; i--) {
-            ans[i] = pq.poll().getKey();
+        int[] ans = new int[k];
+        for (int i=0; i<k; i++) {
+            ans[i] = elements[i].number;
         }
         
         return ans;
+    }
+    
+    private void quickSelect(Element[] elements, int low, int high, int k) {
+        // System.out.println(low + ":" + high + ":" + k);
+        int index = partition(elements, low, high);
+        if (index == k) return;
+        if (index > k) quickSelect(elements, low, index-1, k);
+        if (index < k) quickSelect(elements, index+1, high, k);
+    }
+    
+    private int partition(Element[] elements, int low, int high) {
+        
+        if (low == high) return low;
+        int i = low + 1, j = high;
+        
+        // 3, 3, 3
+        while (i < j) {
+            
+            while (i <= j && elements[i].frequency > elements[low].frequency) i++;
+
+            while (i < j && elements[j].frequency <= elements[low].frequency) j--;
+
+            if (i < j) {
+                swap(elements, i, j);    
+            }
+        }
+        
+        swap(elements, low, i-1);
+        
+        return i-1;
+    }
+    
+    private void swap(Element[] elements, int i, int j) {
+        Element temp = elements[i];
+        elements[i] = elements[j];
+        elements[j] = temp;
     }
 }
