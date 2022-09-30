756230e17640ce7c0ed50e7909de9d294afff299
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-09-30 14:34:29 -0700

    Time: 17 ms (12.60%), Space: 42.5 MB (33.63%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,37 +1,53 @@
 class Solution {
     public int shortestWay(String source, String target) {
+        int m = source.length();
+        
+        
+        int[][] memo = new int[m][26];
+        
+        Arrays.fill(memo[m-1], -1);
+        memo[m-1][source.charAt(m-1) - 'a'] = m-1;
+        
+        for (int i=m-2; i>=0; i--) {
+            for (int j=0; j<26; j++) {
+                memo[i][j] = memo[i+1][j];
+            }
+            memo[i][source.charAt(i) - 'a'] = i;
+        }
+        
         int current = 0;
         int count = 1;
+        
         for (int i=0; i<target.length(); i++) {
             
             boolean charFound = false;
-            while (current < source.length()) {
-                if (source.charAt(current) == target.charAt(i)) {
-                    current++;
-                    charFound = true;
-                    break;
-                }
-                current++;
+            
+            if (current == source.length()) {
+                count++;
+                current = 0;
             }
             
-            if (!charFound) {
+            int index = memo[current][target.charAt(i) - 'a'];
+            
+            
+            if (index >= 0) {
+                current = index+1;
+                charFound = true;
+            } else {
                 current = 0;
                 count++;
                 
-                while (current < source.length()) {
-                    if (source.charAt(current) == target.charAt(i)) {
-                        current++;
-                        charFound = true;
-                        break;
-                    }
-                    current++;
+                index = memo[current][target.charAt(i) - 'a'];
+                
+                if (index >= 0) {
+                    current = index+1;
+                    charFound = true;
                 }
             }
             
             if (!charFound) {
                 return -1;
             }
-            
         }
         
         return count;
