c99cb86c284e984fe37cbd0087d543a058990aef
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-09-30 18:02:54 -0700

    Time: 80 ms (42.28%), Space: 95.6 MB (21.58%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -0,0 +1,37 @@
+class SnapshotArray {
+    
+    Map<Integer, TreeMap<Integer, Integer>> indexToSnapIdtoValueMap;
+    int currentSnapId = 0;
+    
+    public SnapshotArray(int length) {
+        indexToSnapIdtoValueMap = new HashMap<>();
+        for (int i=0; i<length; i++) {
+            TreeMap<Integer, Integer> snapIdToValueMap = new TreeMap<>();
+            snapIdToValueMap.put(0, 0);
+            indexToSnapIdtoValueMap.put(i, snapIdToValueMap);
+        }
+    }
+    
+    public void set(int index, int val) {
+        indexToSnapIdtoValueMap.get(index).put(currentSnapId, val);
+    }
+    
+    public int snap() {
+        currentSnapId++;
+        return currentSnapId-1;
+    }
+    
+    public int get(int index, int snap_id) {
+        // System.out.println(indexToSnapIdtoValueMap);
+        // System.out.println(index + ":" + snap_id);
+        return indexToSnapIdtoValueMap.get(index).floorEntry(snap_id).getValue();
+    }
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */
