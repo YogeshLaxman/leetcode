18eed717224522fc50cc4e921205913d9bbf36fa
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-09-30 18:03:42 -0700

    Time: 87 ms (34.74%), Space: 111.1 MB (5.11%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.java
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -3,6 +3,7 @@ class SnapshotArray {
     Map<Integer, TreeMap<Integer, Integer>> indexToSnapIdtoValueMap;
     int currentSnapId = 0;
     
+    // O(length)
     public SnapshotArray(int length) {
         indexToSnapIdtoValueMap = new HashMap<>();
         for (int i=0; i<length; i++) {
@@ -12,15 +13,18 @@ class SnapshotArray {
         }
     }
     
+    // O(logn)
     public void set(int index, int val) {
         indexToSnapIdtoValueMap.get(index).put(currentSnapId, val);
     }
     
+    //O(1)
     public int snap() {
         currentSnapId++;
         return currentSnapId-1;
     }
     
+    // O(logn)
     public int get(int index, int snap_id) {
         // System.out.println(indexToSnapIdtoValueMap);
         // System.out.println(index + ":" + snap_id);
