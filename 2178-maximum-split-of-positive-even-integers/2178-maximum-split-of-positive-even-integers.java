72f33b1cc615ecf00494bdf529323ac0b9c481bb
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-09-30 19:02:45 -0700

    Time: 28 ms (45.88%), Space: 134.5 MB (27.97%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Long> maximumEvenSplit(long finalSum) {
-        Set<Long> ans = new HashSet<>();
+        List<Long> ans = new ArrayList<>();
         
         if (finalSum % 2 == 1) 
             return new ArrayList<>();;
@@ -8,19 +8,18 @@ class Solution {
         long current = 2;
         long totalSum = 0;
         
-        while (totalSum < finalSum) {
+        while (totalSum + current <= finalSum) {
             ans.add(current);
             totalSum += current;
             current += 2;
-            
-            if (totalSum > finalSum) {
-                long diff = totalSum - finalSum;
-                ans.remove(diff);
-                totalSum = finalSum;
-            }
         }
         
-        return new ArrayList<>(ans);
+        if (totalSum < finalSum) {
+            long last = ans.remove(ans.size()-1);
+            ans.add(last + finalSum - totalSum);
+        }
+        
+        return ans;
         
     }
 }
