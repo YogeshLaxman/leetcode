d495dbc2ff00f1b908e4fe941ea9b7c214ffe09c
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-10-02 19:19:57 -0700

    Time: 33 ms (44.93%), Space: 49.2 MB (9.17%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,19 +1,86 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
         
-        long sum = 0;
+        Stack<int[]> stack = new Stack<>();
+        
         for (int i=0; i<n; i++) {
-            int min = nums[i];
-            int max = nums[i];
-            for (int j=i; j<n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                
-                sum += (max - min);
+            int[] pair = new int[] {nums[i], 1};
+            while (!stack.isEmpty()) {
+                if (stack.peek()[0] > nums[i]) {
+                    int[] popped = stack.pop();
+                    pair[1] += popped[1];
+                } else {
+                    break;
+                }
             }
+            stack.push(pair);
+            left[i] = pair[1];
         }
         
-        return sum;
+        stack.clear();
+        for (int i=n-1; i>=0; i--) {
+            int[] pair = new int[] {nums[i], 1};
+            while (!stack.isEmpty()) {
+                if (stack.peek()[0] >= nums[i]) {
+                    int[] popped = stack.pop();
+                    pair[1] += popped[1];
+                } else {
+                    break;
+                }
+            }
+            stack.push(pair);
+            right[i] = pair[1];
+        }
+        
+        double minSum = 0;
+        for (int i=0; i<n; i++) {
+            minSum += (1l * left[i] * right[i] * nums[i]);
+        }
+        // System.out.println(Arrays.toString(left));
+        // System.out.println(Arrays.toString(right));
+        stack.clear();
+        Arrays.fill(left, 0);
+        Arrays.fill(right, 0);
+        
+        for (int i=0; i<n; i++) {
+            int[] pair = new int[] {nums[i], 1};
+            while (!stack.isEmpty()) {
+                if (stack.peek()[0] < nums[i]) {
+                    int[] popped = stack.pop();
+                    pair[1] += popped[1];
+                } else {
+                    break;
+                }
+            }
+            stack.push(pair);
+            left[i] = pair[1];
+        }
+        
+        stack.clear();
+        for (int i=n-1; i>=0; i--) {
+            int[] pair = new int[] {nums[i], 1};
+            while (!stack.isEmpty()) {
+                if (stack.peek()[0] <= nums[i]) {
+                    int[] popped = stack.pop();
+                    pair[1] += popped[1];
+                } else {
+                    break;
+                }
+            }
+            stack.push(pair);
+            right[i] = pair[1];
+        }
+        
+        double maxSum = 0;
+        
+        for (int i=0; i<n; i++) {
+            maxSum += (1l * left[i] * right[i] * nums[i]);
+        }
+        
+        // System.out.println(maxSum + ":" + minSum);
+        return (long) (maxSum - minSum);
     }
 }
