571824225562a6ea5411059a048a0e14ec88c12b
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-10-03 12:11:27 -0700

    Time: 21 ms (62.65%), Space: 142.5 MB (47.92%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+        int n = s.length();
+        
+        int[] left = new int[n];
+        int prev = -1;
+        for (int i=0; i<n; i++) {
+            if (s.charAt(i) == '|') {
+                prev = i;
+            }
+            left[i] = prev;
+        }
+        
+        int[] right = new int[n];
+        prev = -1;
+        for (int i=n-1; i>=0; i--) {
+            if (s.charAt(i) == '|') {
+                prev = i;
+            }
+            right[i] = prev;
+        }
+        
+        int[] prefixSum = new int[n];
+        int sum = 0;
+        for (int i=0; i<n; i++) {
+            if (s.charAt(i) == '*') {
+                sum += 1;
+            }
+            prefixSum[i] = sum;
+        }
+        
+        int m = queries.length;
+        int[] result = new int[m];
+        
+        for (int i=0; i<m; i++) {
+            int leftIndex = right[queries[i][0]];
+            int rightIndex = left[queries[i][1]];
+            
+            if (leftIndex == -1 || rightIndex == -1 || leftIndex > queries[i][1] || rightIndex < queries[i][0]) {
+                continue;
+            }
+            
+            result[i] = prefixSum[rightIndex] - prefixSum[leftIndex];
+        }
+        
+        return result;
+    }
+}
