2a0e180fa63f010b20e05ad1e3d5781bbe38d630
Yogesh Laxman <47672527+YogeshLaxman@users.noreply.github.com>
2022-10-03 20:00:20 -0700

    Time: 124 ms (15.79%), Space: 112.9 MB (83.23%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         // return kClosestMinPQ(points, k);
-        return kClosestMaxPQ(points, k);
+        // return kClosestMaxPQ(points, k);
+        return kClosestQuickSelect(points, k);
     }
     
     // min priority queue - (nlogn)
@@ -50,6 +51,50 @@ class Solution {
     }
     
     private int[][] kClosestQuickSelect(int[][] points, int k) {
-        return null;
+        quickSelect(points, k-1, 0, points.length-1);
+        
+        int[][] result = new int[k][];
+        for (int i=0; i<k; i++) {
+            result[i] = points[i];
+        }
+        return result;
+    }
+    
+    private void quickSelect(int[][] points, int k, int low, int high) {
+        int index = findPivot(points, low, high);
+        
+        if (index == k) return;
+        
+        if (k < index) {
+            quickSelect(points, k, low, index-1);
+        } else {
+            quickSelect(points, k, index+1, high);
+        }
+    }
+    
+    private int findPivot(int[][] points, int low, int high) {
+        int i = low, j = low+1;
+        
+        while (j <= high) {
+            if (distance(points[j]) <= distance(points[low])) {
+                swap(points, i+1, j);
+                i++;
+            }
+            j++;
+        }
+        swap(points, i, low);
+        
+        return i;
+    }
+    
+    private int distance(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+    
+    private void swap(int[][] points, int index1, int index2) {
+        // System.out.println(index1 + ":" + index2);
+        int[] temp = points[index1];
+        points[index1] = points[index2];
+        points[index2] = temp;
     }
 }
